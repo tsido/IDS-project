ec793ed2095fd29c7fd8a1b83c837c7c8b0da8ff
Teemu Sidoroff <tsido@iki.fi>
2022-10-20 10:07:59 +0300

    Output report to separate directory

--- create_report.sh
+++ create_report.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jupyter nbconvert --to HTML GameCrashPost.ipynb --embed-images --template lab --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags hide_code
+jupyter nbconvert --to HTML GameCrashPost.ipynb --embed-images --template lab --TagRemovePreprocessor.enabled=True --TagRemovePreprocessor.remove_cell_tags hide_code --output-dir pub
